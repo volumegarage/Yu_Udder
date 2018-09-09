ea998997dde2406f52f2f7bbccb4e28a24eb0f31
David Cate <davidcate@imac-2.local>
2018-09-09 12:56:16 -0400

    Initial Commit

---
+++ Yu_Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Yu_Udder
+//
+//  Created by David Cate on 9/9/18.
+//  Copyright © 2018 David Cate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
